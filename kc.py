b1560dae393420ea78dbbc07d7d98937f526ee33
Rajnandini Mukherjee <rajnandinimukherjee@Rajnandinis-MacBook-Air.local>
2021-09-06 14:30:50 +0100

    kc added

--- kc.py
+++ kc.py
@@ -54,6 +54,8 @@ def del_RC(matrix, row, col):
 def M11(known_ts, known_ffs, X, **kwargs):
     N = len(known_ts)
     phi_ff_vec = phi(np.array(known_ts),**kwargs)*known_ffs
+    #phi_ff_vec = np.array([phi(known_ts[i],**kwargs)*known_ffs[i]
+    #                       for i in range(N)])
 
     top_line = np.hstack((X, phi_ff_vec))
     g00 = G(known_ts)
@@ -63,9 +65,9 @@ def M11(known_ts, known_ffs, X, **kwargs):
     return M11
 
 from numpy.linalg import det
-def bounds(unknown_t, known_ts, known_ffs, X, **kwargs):
+def bounds(unknown_t, known_ts, known_ffs, X, prnt=False, **kwargs):
     N = len(known_ts)
-    g = G(np.hstack((unknown_t,known_ts)))
+    g = G(np.hstack((unknown_t, known_ts)))
 
     alpha = det(del_RC(g,0,0))
     beta = sum([((-1)**(j+1))*phi(known_ts[j],**kwargs)*known_ffs[j]*det(del_RC(g,0,j+1))
@@ -75,10 +77,12 @@ def bounds(unknown_t, known_ts, known_ffs, X, **kwargs):
                             for j in range(N)] for i in range(N)])
     gamma = X*det(g)-np.sum(gamma_mtx)
 
-    discriminant = np.sqrt((beta**2)+(alpha*gamma))
-    upper_bound = (-beta+discriminant)/(alpha*phi(unknown_t,**kwargs))
-    lower_bound = (-beta-discriminant)/(alpha*phi(unknown_t,**kwargs))
+    disc = ((beta**2)+(alpha*gamma))
+    upper_bound = (-beta+(disc**0.5))/(alpha*phi(unknown_t,**kwargs))
+    lower_bound = (-beta-(disc**0.5))/(alpha*phi(unknown_t,**kwargs))
 
+    if prnt:
+        print((lower_bound, upper_bound), disc)
     return [lower_bound, upper_bound]
 
 def bootstrap(var, cov, K=100, **kwargs):
@@ -114,53 +118,110 @@ X, X_err = np.array([X_zero,X_plus]), np.array([X_zero_err, X_plus_err])
 dict_zero = {'t_p':2.3178, 'ff':'0'}
 dict_plus = {'t_p':2.1122, 'ff':'+'}
 
-N_boot = 1000
+t_range = np.arange(0,t_minus,0.01)+0.01
+N_boot = 100
+N_0 = 10
 samples = bootstrap(known_ffs, COV_input, K=N_boot)
 samples_X = bootstrap(X,np.diag(X_err)**2,K=N_boot)
+
+zero_dist = {str(t):{'up':[], 'lo':[]} for t in t_range}
+plus_dist = {str(t):{'up':[], 'lo':[]} for t in t_range} 
 accepted_idx = []
-accepted_bounds = []
+
+import time
+t1 = time.time()
 for k in range(N_boot):
     m11_zero = M11(known_ts, samples[k,:3], samples_X[k,0], **dict_zero)
     m11_plus = M11(known_ts, samples[k,3:], samples_X[k,1], **dict_plus)
-    if det(m11_zero)>=0 and det(m11_plus)>0:
+    if det(m11_zero)>0 and det(m11_plus)>0:
         [zero_low, zero_up] = bounds(0, known_ts, samples[k,:3], samples_X[k,0],
                                      **dict_zero)
         [plus_low, plus_up] = bounds(0, known_ts, samples[k,3:], samples_X[k,1],
                                      **dict_plus)
         if zero_up>plus_low and plus_up>zero_low: # kinematical constraint
-            accepted_bounds.append([max(zero_low, plus_low), min(zero_up, plus_up)])
             accepted_idx.append(k)
-
-N_acc = len(accepted_idx)
-accepted_samples = samples[accepted_idx,:]
-accepted_X = samples_X[accepted_idx,:]
-
-def constrained_bounds(unknown_t, known_ts, known_ffs, X, **kwargs):
-    N_0 = 100
-    f_dist = []
-    for k in range(N_acc):
-        f0s = np.random.uniform(accepted_bounds[k][0], accepted_bounds[k][1], N_0)
-        bnds= []
-        for n in range(N_0):
+            np.random.seed(1)
+            f0s = np.random.uniform(max(zero_low,plus_low), min(zero_up,plus_up), N_0)
             known_ts_0 = np.hstack((known_ts,0))
-            known_ffs_0 = np.hstack((known_ffs,f0s[n]))
-            bnds.append(bounds(unknown_t, known_ts_0, known_ts_0, X,
-                                        **kwargs))
-        bnds = np.array(bnds)
-        f_dist.append([min(bnds[:,0]), max(bnds[:,1])])
-    
-    return f_dist
-        
-def bounds_with_errors(t_range, **kwargs):
-    t_dist = []
-    for t in t_range:
-        t_dist.append(constrained_bounds(unknown_t=t,**kwargs))
+            
+            for t in t_range:
+                zero_bounds = np.array([bounds(t,known_ts_0,
+                                np.hstack((samples[k,:3],f0s[n])),
+                                samples_X[k,0],**dict_zero)
+                                for n in range(N_0)])
 
-    return np.array(t_dist)
+                plus_bounds = np.array([bounds(t,known_ts_0,
+                                np.hstack((samples[k,3:],f0s[n])),
+                                samples_X[k,1],**dict_plus)
+                                for n in range(N_0)])
 
-t_range = np.arange(0,t_minus,0.01)
-t_dist_zero = bounds_with_errors(t_range, known_ts=known_ts, known_ffs=ffs_zero,
-                                X=X_zero, **dict_zero)
+                zero_dist[str(t)]['lo'].append(np.min(zero_bounds[:,0]))
+                zero_dist[str(t)]['up'].append(np.min(zero_bounds[:,1]))
+                plus_dist[str(t)]['lo'].append(np.min(plus_bounds[:,0]))
+                plus_dist[str(t)]['up'].append(np.min(plus_bounds[:,1]))
+
+print('Time taken:',time.time()-t1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#N_acc = len(accepted_idx)
+#accepted_samples = samples[accepted_idx,:]
+#accepted_X = samples_X[accepted_idx,:]
+#
+#def constrained_bounds(unknown_t, known_ts, known_ffs, X, **kwargs):
+#    N_0 = 100
+#    f_dist = []
+#    for k in range(N_acc):
+#        f0s = np.random.uniform(accepted_bounds[k][0], accepted_bounds[k][1], N_0)
+#        bnds= []
+#        for n in range(N_0):
+#            known_ts_0 = np.hstack((known_ts,0))
+#            known_ffs_0 = np.hstack((known_ffs,f0s[n]))
+#            bnds.append(bounds(unknown_t, known_ts_0, known_ts_0, X,
+#                                        **kwargs))
+#        bnds = np.array(bnds)
+#        f_dist.append([min(bnds[:,0]), max(bnds[:,1])])
+#    
+#    return f_dist
+#        
+#def bounds_with_errors(t_range, **kwargs):
+#    t_dist = []
+#    for t in t_range:
+#        t_dist.append(constrained_bounds(unknown_t=t,**kwargs))
+#
+#    return np.array(t_dist)
+#
+#t_dist_zero = bounds_with_errors(t_range, known_ts=known_ts, known_ffs=ffs_zero,
+#                                X=X_zero, **dict_zero)
 
 
 
